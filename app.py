9c904ea940f25a756e2b73ffd601ea323381c0ad
William Sayer <wmsayer3@gmail.com>
2020-03-08 15:04:53 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-import os
-import sys
+# import os
+# import sys
+#
+#
+# sys.path.insert(0, os.path.dirname(__file__))
+#
+#
+# def app(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     message = 'It works!\n'
+#     version = 'Python v' + sys.version.split()[0] + '\n'
+#     response = '\n'.join([message, version])
+#     return [response.encode()]
 
+from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
 
-sys.path.insert(0, os.path.dirname(__file__))
+app = Flask(__name__)
 
+# app.config['MYSQL_USER'] = 'cs411ccsquad_admin'
+# app.config['MYSQL_PASSWORD'] = 'password;uiuc'
+app.config['MYSQL_HOST'] = 'cs411ccsquad.web.illinois.edu'
+app.config['MYSQL_DB'] = 'cs411ccsquad_FlicksNDrinks'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
-    version = 'Python v' + sys.version.split()[0] + '\n'
-    response = '\n'.join([message, version])
-    return [response.encode()]
+mysql = MySQL(app)
+
+@app.route('/')
+def Home():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT cocktailName FROM CocktailName WHERE cocktailId < 10")
+    fetchdata = cur.fetchall()
+    cur.close()
+    return fetchdata
