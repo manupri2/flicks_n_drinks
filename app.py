ed391ad0e774d5a3b95db59992f043bbcaca6f76
William Sayer <wmsayer3@gmail.com>
2020-03-08 18:00:53 -0500

    update with run

--- app.py
+++ app.py
@@ -85,17 +85,18 @@ app = Flask(__name__)
 # app.config['MYSQL_USER'] = 'cs411ccsquad_admin'
 # app.config['MYSQL_PASSWORD'] = 'password;uiuc'
 # app.config['MYSQL_DB'] = 'cs411ccsquad_FlicksNDrinks'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://cs411ccsquad_admin:password;uiuc@localhost/cs411ccsquad_FlicksNDrinks'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://cs411ccsquad_admin:password;uiuc@localhost/cs411ccsquad_FlicksNDrinks'
 # engine = create_engine('mysql+pymysql://cs411ccsquad_admin:password;uiuc@localhost/cs411ccsquad_FlicksNDrinks', echo=True)
 db = SQLAlchemy(app)
-engine = db.engine
-print(engine)
 
 
 @app.route('/')
 def Home():
     print("HELLLLLLLLLLLOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
-    conn = engine.connect()
+    engine = db.engine
+    print(engine)
+    print("HELLLLLLLLLLLOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+    conn = db.engine.connect()
     print(conn)
     print("HELLLLLLLLLLLOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
     print(conn)
