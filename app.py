7fab48fc3fcc654ea37e5585d7903ece454f51ee
William Sayer <wmsayer3@gmail.com>
2020-03-08 14:14:42 -0500

    uploading app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # app.config['MYSQL_USER'] = 'cs411ccsquad_admin'
 # app.config['MYSQL_PASSWORD'] = 'password;uiuc'
-app.config['MYSQL_HOST'] = 'cs411ccsquad.web.illinois.edu'
+app.config['MYSQL_HOST'] = 'cs411ccsquad.web.illinois.edu/'
 app.config['MYSQL_DB'] = 'cs411ccsquad_FlicksNDrinks'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
