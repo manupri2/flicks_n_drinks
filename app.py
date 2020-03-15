d32a359189052eecc9376b152165d4737328ed6c
William Sayer <wmsayer3@gmail.com>
2020-03-15 12:46:52 -0500

    new api

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
-import os
-import sys
-import sqlalchemy
-from flask import Flask, render_template, request
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
-
-# eng = sqlalchemy.create_engine('mysql://cs411ccsquad_admin:password;uiuc@localhost/cs411ccsquad_FlicksNDrinks')
+from flask_restful import Resource, Api
+from json import dumps
+from flask_jsonpify import jsonify
+import urllib.parse
 
 id_dict = {'CocktailName': 'cocktailId',
             'CocktailRecipe': 'recipeId',
             'Ingredient': 'ingredientId'}
-
-# 0-683, 0-686, 0-1223
+# Table_id ranges: 0-683, 0-686, 0-1223
 
 app = Flask(__name__)
-
-app.config[
-    'SQLALCHEMY_DATABASE_URI'] = 'mysql://cs411ccsquad_admin:password;uiuc@localhost/cs411ccsquad_FlicksNDrinks'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://cs411ccsquad_admin:password;uiuc@localhost/cs411ccsquad_FlicksNDrinks'
 db = SQLAlchemy(app)
 eng = db.engine
+# api = Api(app)
+
+# class Access_FlicksNDrinks(Resource):
+#     def get(self, query):
+#         conn = eng.connect()
+#         query_data = conn.execute(urllib.parse.unquote(query))
+#         result = {'data': [dict(zip(tuple(query_data.keys()), i)) for i in query_data.cursor]}
+#         return jsonify(result)
 
 
 @app.route('/')
 def stanford_page():
-    return """<h1>To query database, enter "CocktailName", "CocktailRecipe", or "Ingredient" for {table_name} and an integer between 0-683 for {id_num} in the route:    http://cs411ccsquad.web.illinois.edu/{table_name}/{id_num}</h1>"""
+    return """<h1>To query database, enter "CocktailName", "CocktailRecipe", or "Ingredient" for {table_name} and 
+              an integer between 0-683 for {id_num} in the route:    
+              http://cs411ccsquad.web.illinois.edu/{table_name}/{id_num}</h1>"""
 
 
 @app.route('/<table_name>/<id_num>')
@@ -34,26 +39,24 @@ def Home(table_name, id_num):
         # cur = con.execute('SELECT * FROM CocktailName')
         for i in cur:
             message2 += repr(i) + "\n"
-
     return message2.encode()
 
 
-# @app_obj.route('/')
-# def app(environ, start_response):
-#     start_response('200 OK', [('Content-Type', 'text/plain')])
+@app.route('/API_SQL/<query>', methods=['GET'])
+def api_sql(self, query):
+    conn = eng.connect()
+    if request.method == 'GET':
+        # query = request.values.get('query')
+        query_data = conn.execute(urllib.parse.unquote(query))
+        result = {'data': [dict(zip(tuple(query_data.keys()), i)) for i in query_data.cursor]}
+        return jsonify(result)
 
-#     message = ''
-#     with eng.connect() as con:
-#         cur = con.execute('SELECT * FROM Role')
-#         for i in cur:
-#             message += repr(i) + "\n"
+# api.add_resource(Access_FlicksNDrinks, '/SQL_QUERY/<query>')
+    # import urllib.parse
+    # query = 'Hellö Wörld@Python'
+    # urllib.parse.quote(query) # encodes query into URL format, returns encoded string
+    # urllib.parse.unquote(encodedStr) # decodes query from URL format, returns decoded string
 
-#     message += '\nIt works!\n'
-#     version = 'Python v' + sys.version.split()[0] + '\n'
-#     response = '\n'.join([message, version])
-
-#     return [response.encode()]
-# app = app_obj.run
 
 if __name__ == '__main__':
     app.run(use_reloader=True)
