1c0b89e4dfb20f76c21e639ea2d989b05fc4ad93
William Sayer <wmsayer3@gmail.com>
2020-03-16 20:32:35 -0500

    update app.py

--- build_personality_to_movie_training_set.py
+++ build_personality_to_movie_training_set.py
@@ -4,21 +4,13 @@ import pandas as pd
 import re
 from urllib import parse
 import requests
+from sql_api import api_query
 
 
-movie_trait_matrix = pd.read_csv('./static/MovieTraitMatrix.csv')
+# movie_trait_matrix = pd.read_csv('./static/MovieTraitMatrix.csv')
 # cocktail_raw['recipeId'] = range(0, cocktail_raw.shape[0])
 # categorized_ing.set_index('word', inplace=True)
-
-
-test_query = "SELECT * FROM CocktailName"
-encoded_query = parse.quote(test_query)
-print(encoded_query)
-print(parse.unquote(encoded_query))
-route = "http://cs411ccsquad.web.illinois.edu/api/%s" % encoded_query
-print(route)
-response = requests.get(route)
-queried_data_json = response.json()
-print(queried_data_json)
-queried_data_df = pd.read_json(repr(queried_data_json), orient='records')
-print(queried_data_df)
+query = 'SELECT * FROM Composition WHERE compositionId > 5 AND compositionId < 100'
+df, code = api_query(query)
+print(code)
+print(df)
---
+++ sql_api.py
@@ -0,0 +1,58 @@
+import pandas as pd
+from urllib import parse
+import requests
+
+
+def api_query(query_str):
+    encoded_query = parse.quote(query_str)
+    # print(encoded_query)
+    # print(parse.unquote(encoded_query))
+    route = "http://cs411ccsquad.web.illinois.edu/api/%s" % encoded_query
+    # print(route)
+    response = requests.get(route)
+
+    queried_data_df = pd.DataFrame()
+    if response.status_code == 200:
+        queried_data_json = response.json()
+        # print(queried_data_json)
+        queried_data_df = pd.DataFrame(queried_data_json)
+        # print(queried_data_df)
+    return queried_data_df, response.status_code
+
+
+def test_df(tables):
+    for table in tables:
+        test_query = "SELECT * FROM %s" % table
+        test_df, code = api_query(test_query)
+        if code == 200:
+            print('Table %s was a success!' % table)
+        else:
+            print('Table %s was a fail!' % table)
+
+
+if __name__ == "__main__":
+    table_list = ['CocktailDecoration',
+    'CocktailName',
+    'CocktailRecipe',
+    'Composition',
+    'Connection',
+    'Crew',
+    'FavoriteCocktail',
+    'FavoriteMovie',
+    'FavoritePair',
+    'Garnish',
+    'Genre',
+    'Glassware',
+    'Ingredient',
+    'IngredientClassification',
+    'IngredientType',
+    'Movie',
+    'MovieCategory',
+    'MovieCocktailPairing',
+    'People',
+    'Role',
+    'User']
+    test_df(table_list)
+
+
+
