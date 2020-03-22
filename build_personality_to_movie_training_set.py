4474d072a6b9f056a69e37e05e4b759f67cd2aa4
William Sayer <wmsayer3@gmail.com>
2020-03-22 13:23:18 -0500

    initial code for building training set

--- build_personality_to_movie_training_set.py
+++ build_personality_to_movie_training_set.py
@@ -1,19 +1,45 @@
 from io import StringIO
 import csv
 import pandas as pd
+import numpy as np
 import re
 from urllib import parse
 import requests
 from sql_api import api_query
 
+query = 'SELECT Movie.tConst FROM Movie'
+movie_ids, code = api_query(query)
 
-# movie_trait_matrix = pd.read_csv('./static/MovieTraitMatrix.csv')
+query = 'SELECT MovieCategory.tConst, MovieCategory.genreId, Genre.genreName' \
+        ' FROM MovieCategory' \
+        ' INNER JOIN Genre ON MovieCategory.genreId = Genre.genreId'
+movie_cat, code = api_query(query)
+movie_cat.set_index('tConst', inplace=True)
+
+num_users = 500
+num_movies = 100
+total_num_movies = movie_ids.shape[0]
+user_ids = np.linspace(0, num_users-1, num_users)
+opennness = np.random.uniform(0, 100, size=user_ids.shape)
+conscientiousness = np.random.uniform(0, 100, size=user_ids.shape)
+extraversion = np.random.uniform(0, 100, size=user_ids.shape)
+agreeableness = np.random.uniform(0, 100, size=user_ids.shape)
+neuroticism = np.random.uniform(0, 100, size=user_ids.shape)
+user_array = np.array([user_ids, opennness, conscientiousness, extraversion, agreeableness, neuroticism])
+print(user_array.shape)
+# training_set = pd.DataFrame
+# for user in
+
+
+
+
+# print(user_ids)
+# print(opennness)
+
+movie_trait_matrix = pd.read_csv('./static/MovieTraitMatrix.csv')
 # cocktail_raw['recipeId'] = range(0, cocktail_raw.shape[0])
 # categorized_ing.set_index('word', inplace=True)
+print(movie_trait_matrix)
 
 
-query = 'SELECT * FROM Composition WHERE compositionId > 5 AND compositionId < 100'
-df, code = api_query(query)
-print(code)
-print(df)
 
