a8b85bf785f0572d6be83c9e2ce788e826b73910
William Sayer <wmsayer3@gmail.com>
2020-04-04 19:42:25 -0500

    WS final app try

--- application/static/js/aboutPage.jsx
+++ application/static/js/aboutPage.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 require('../css/fullstack.css');
 
+
 function validateForm() {
   document.getElementById('my_button').value += "1";
   if (x == "") {
