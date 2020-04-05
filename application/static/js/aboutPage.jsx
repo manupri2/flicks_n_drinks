b4a8b1147fa20746c84084885000f6406851e431
William Sayer <wmsayer3@gmail.com>
2020-04-04 19:15:43 -0500

    WS final app try

--- application/static/js/aboutPage.jsx
+++ application/static/js/aboutPage.jsx
@@ -2,4 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 require('../css/fullstack.css');
 
-
+function validateForm() {
+  var x = document.forms["myForm"]["table"].value;
+  if (x == "") {
+    alert("Table name must be filled out!!");
+    return false;
+  }
+}
