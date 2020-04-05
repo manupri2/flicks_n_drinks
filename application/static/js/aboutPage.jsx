9d56d5fa10cbdaa7118198505da79382cce2ac67
William Sayer <wmsayer3@gmail.com>
2020-04-04 19:26:06 -0500

    WS final app try

--- application/static/js/aboutPage.jsx
+++ application/static/js/aboutPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 require('../css/fullstack.css');
 
 function validateForm() {
-  var x = document.forms["myForm"]["table"].value;
+  document.getElementById('my_button').value += "1";
   if (x == "") {
     alert("Table name must be filled out!!");
     return false;
+  }
+  else{
+       ReactDOM.render(<App />, document.getElementById("content"));
   }
 }
