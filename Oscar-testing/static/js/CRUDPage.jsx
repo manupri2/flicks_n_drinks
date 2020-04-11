105bcacb3cb1dc8d5309826369188eb84e0c3e30
oscarhuang1212 <oscarhuang1212@gmail.com>
2020-04-11 00:16:01 -0500

    stage Four All functions

---
+++ Oscar-testing/static/js/CRUDPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+require('../css/fullstack.css');
+import CRUDApp from './CRUDApp';
+
+ReactDOM.render(<CRUDApp />, document.getElementById('root'));
