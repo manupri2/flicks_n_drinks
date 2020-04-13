06f13ad5ee1ee164f2854b06c2626ba9697e3f84
manupriyaarora <manupri2@illinois.edu>
2020-04-12 21:00:47 -0500

    Added a new link on home page

--- application/static/js/loginPage.jsx
+++ application/static/js/loginPage.jsx
@@ -6,7 +6,6 @@ var x = document.getElementById("login");
 var y = document.getElementById("register");
 var z = document.getElementById("btn");
 
-
 function  register() {
     x.style.left = "-400px";
     y.style.left = "50px";
